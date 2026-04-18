3d87f991c49b35cd31525e89b0d07be6ce1a089a
aphTu <aptu27@gmail.com>
2026-04-18 11:57:21 -0700

    created constants.py, and added all the supported file type of the program

---
+++ constants.py
@@ -0,0 +1,23 @@
+from enum import Enum
+class GenericFileType(Enum):
+  TEXT = "text"
+  IMAGE= "image"
+  AUDIO = "audio"
+  VIDEO = "video"
+  PROGRAM = "program"
+  COMPRESS = "compress"
+  WEB ="web"
+
+
+
+file_type = {
+  GenericFileType.TEXT : (".txt", ".rtf", ".docx", ".csv", ".doc", ".wps", ".wpd", ".msg"),
+  GenericFileType.IMAGE: (".jpg", ".png", ".webp", ".gif", ".tif", ".bmp", ".eps"),
+  GenericFileType.AUDIO: (".mp3", ".wma", ".snd", ".wav", ".ra", ".au", ".aac"),
+  GenericFileType.VIDEO: (".mp4", ".3gp", ".avi", ".mpg", ".mov", ".wmv"),
+  GenericFileType.PROGRAM: (".c", ".cpp", ".java",".py", ".js",".ts",".cs",".swift",".dta",".pl",".sh",".bat",".exe",".com"),
+  GenericFileType.COMPRESS: (".rar",".zip",".hqx",".arj",".tar",".arc",".sit",".gz",".z"),
+  GenericFileType.WEB: (".html", ".html", ".xhtml", ".asp", ".css", ".aspx", ".rss")
+}
+
+
